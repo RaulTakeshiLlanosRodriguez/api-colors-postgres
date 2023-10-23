c702c88577e45f07ba15775a6e083728769d7571
RaulTakeshiLlanosRodriguez <68862116+RaulTakeshiLlanosRodriguez@users.noreply.github>
2023-10-23 00:18:19 -0500

    first commit

---
+++ src/main/java/com/utp/colors/ApiColorsApplication.java
@@ -0,0 +1,13 @@
+package com.utp.colors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiColorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiColorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utp/colors/ApiColorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.colors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiColorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
